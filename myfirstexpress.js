74086b2df455a2c87a7ecc344bc2c3a05c413147
Feta Jordan <fetajordan6@gmail.com>
2025-10-15 10:37:30 +0300

    Express

--- myfirstexpress.js
+++ myfirstexpress.js
@@ -1,17 +1,23 @@
-//Import express
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 //Middelware to parse JSON bodies
 app.use(express.json());
 
 //Basic route
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}')
+})
 //start server
 app.listen(port, () => {
   console.log('Server running at http://localhost:${port}/');
+});
+//Route to handle GET requests
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
 });
